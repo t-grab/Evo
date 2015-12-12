b20382a14693396ba635a6e14771496f9ed7fb23
t-grab <tobias.grabosch@oh22.net>
2015-12-12 11:32:56 +0100

    Reworked FitnessProportionalSelection based on STD algorithms

--- src/hpp/Select/fitnessProportionalSelection.hpp
+++ src/hpp/Select/fitnessProportionalSelection.hpp
@@ -8,51 +8,32 @@ using std::mt19937_64;
 using std::uniform_real_distribution;
 using std::accumulate;
 using std::lower_bound;
+using std::partial_sum;
 
 namespace Evo {
 	namespace Select {
 		class FitnessProportional {
 			private:
-				mt19937_64 generator;
-				uniform_real_distribution<double> generate;
-									
-				vector<uint> choose(const vector<double>& probs, uint number) {
-					vector<uint> chosen(number);
-					for (uint i(0U); i < number; ++i)
-						chosen[i] = lower_bound(probs.begin(), probs.end(), generate(generator)) - probs.begin();
-					
-					return chosen;
-				}					
+				mt19937_64 generator;					
 				
 			public:
-				FitnessProportional() : generate(0.0, 1.0), generator(100) {}
+				FitnessProportional() : generator(42) {}
 				
 				template<class Iter>
 				vector<uint> operator()(Iter begin, Iter end, double number) {
-					double total(static_cast<double>(accumulate(begin, end, 0.0)));
+					double total = static_cast<double>(accumulate(begin, end, 0.0));
+					uniform_real_distribution<double> generate(0, total);
 							
 					vector<double> probs(end - begin);
-					probs[0] = *begin / total;
-					for (uint i(1U); i < probs.size(); ++i)
-						probs[i] = probs[i - 1] + *(begin + i) / total;
+					partial_sum(begin, end, probs.begin());
 						
-					return choose(probs, number);
-				}
-				
-				template<class T, class Iter, class Fitness>
-				vector<uint> operator()(Iter begin, Iter end, double number, Fitness fitness) {
-					double total(static_cast<double>(accumulate(begin, end, 0.0, [&](double curr, const T& individual) {
-						return curr + fitness(individual);
-					})));
+					vector<uint> chosen(number);
+					for (auto& idx : chosen)
+						idx = lower_bound(probs.begin(), probs.end(), generate(generator)) - probs.begin();
 					
-					vector<double> probs(end - begin);
-					probs[0] = fitness(*begin) / total;
-					for (uint i(1U); i < probs.size(); ++i)
-						probs[i] = probs[i - 1] + fitness(*(begin + i)) / total;
-					
-					return choose(probs, number);
+					return chosen;
 				}
-		};   	
+		};	
 	}
 }
 
